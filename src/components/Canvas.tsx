e3561c4277c9bcd988d3b6f796341eabcac32f9f
Raymond <raymond.lo@yahoo.com>
2021-11-11 22:20:38 -0500

    Add ref to canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,8 +1,12 @@
+import { useRef } from "react";
+
 type CanvasProp = {
   width?: number;
   height?: number;
 };
 
 export const Canvas = ({ width = 500, height = 500 }: CanvasProp) => {
+  const ref = useRef(null);
+
   return <canvas width={width} height={height} id="canvas"></canvas>;
 };
