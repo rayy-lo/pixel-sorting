e518ed8d73f6348631046285714091a0beabd24a
Raymond <raymond.lo@yahoo.com>
2021-11-11 22:20:27 -0500

    Add CanvasContainer component

---
+++ src/containers/CanvasContainer.tsx
@@ -0,0 +1,9 @@
+import { Canvas } from "../components/Canvas";
+
+export const CanvasContainer = () => {
+  return (
+    <div className="CanvasContainer" style={{ flex: 1 }}>
+      <Canvas />
+    </div>
+  );
+};
