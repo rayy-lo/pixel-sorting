75114c5f57c89888a58ef64d6aa97a33dfcf57c0
Raymond <raymond.lo@yahoo.com>
2021-11-13 00:32:37 -0500

    import app.css

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,10 @@
 import Sidebar from "./Sidebar";
-import styles from "../styles/App.module.css";
+import "../styles/App.css";
 import { CanvasContainer } from "./CanvasContainer";
-
-const { app } = styles;
 
 function App() {
   return (
-    <div className={styles.app}>
+    <div className="App">
       <Sidebar />
       <CanvasContainer />
     </div>
