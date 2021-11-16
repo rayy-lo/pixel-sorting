a3c951d82b4a6e1fd814732c87646b76f4a5e3f1
Raymond <raymond.lo@yahoo.com>
2021-11-15 23:23:50 -0500

    Added file for types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface CanvasProp {
+  /**
+   * Width of canvas
+   * @defaultValue 800
+   */
+  width?: number;
+  /**
+   * Height of canvas
+   * @defaultValue 800
+   */
+  height?: number;
+}
