39ab6312d7cfe23a0858c40df0a5c8b3641f8a86
Raymond <raymond.lo@yahoo.com>
2021-11-18 19:46:47 -0500

    Use in memory canvas to get image data

--- src/containers/Canvas/Canvas.tsx
+++ src/containers/Canvas/Canvas.tsx
@@ -1,11 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import kittenImg from "../../assets/kitten.png";
 import styles from "../../styles/modules/Canvas.module.css";
-import {
-  getInitialImageData,
-  createImageElementForCanvas,
-  shuffleArray,
-} from "./index";
+import { getImageData, createImageElement, shuffleArray } from "./index";
 
 export interface CanvasProp {
   /**
@@ -20,28 +16,26 @@ export interface CanvasProp {
   height?: number;
 }
 
+const image = createImageElement(kittenImg);
+const imageData = getImageData(image);
+
 export const Canvas = ({ width = 800, height = 800 }: CanvasProp) => {
   const { container, canvas } = styles;
-  const [imageData, setImageData] = useState<Uint8ClampedArray | null>(null);
+
+  const [sortedImageData, setSortedImageData] =
+    useState<Uint8ClampedArray>(imageData);
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const intervalRef = useRef<number | null>(null);
 
   useEffect(() => {
-    console.log("first use effect");
     if (canvasRef.current !== null) {
       const context = canvasRef.current.getContext("2d");
 
-      const canvasImage = createImageElementForCanvas(kittenImg);
-      canvasImage.onload = () => {
-        context?.drawImage(canvasImage, 0, 0);
-        const ctxImageData = context?.getImageData(0, 0, 32, 32);
-        if (ctxImageData) setImageData(ctxImageData.data);
-      };
-
-      // intervalRef.current = window.setInterval(() => {
-      //   console.log("Interval function exec");
-      // }, 1000);
+      // canvasImage.onload = () => {
+      //   context?.drawImage(canvasImage, 0, 0);
+      //   const ctxImageData = context?.getImageData(0, 0, 32, 32);
+      // };
     }
   }, []);
 
--- src/containers/Canvas/index.ts
+++ src/containers/Canvas/index.ts
@@ -1,20 +1,46 @@
 /**
- * Get initial ImageData
+ * Get ImageData from image using an in memory canvas
+ *
+ * @param image
+ *
+ * @param tileWidth - Width of each tile
+ *
+ * @param tileHeight - Height of each tile
+ *
+ * @returns Uint8ClampedArray
  */
 
-export const getInitialImageData = () => {};
+export const getImageData = (
+  image: HTMLImageElement,
+  tileWidth: number = 32,
+  tileHeight: number = 32
+): Uint8ClampedArray => {
+  const inMemoryCanvas = document.createElement("canvas");
+  const ctx = inMemoryCanvas.getContext("2d");
+  ctx?.drawImage(image, 0, 0);
+
+  const imageData = ctx?.getImageData(0, 0, tileWidth, tileHeight);
+
+  // return imageData;
+
+  console.log(imageData.data);
+  return new Uint8ClampedArray([]);
+};
 
 /**
  * Generate an image element for Canvas component to render
+ * @async
  *
  * @param src - String source of image
  *
  * @returns HTMLImageElement
  */
 
-export const createImageElementForCanvas = (src: string) => {
+export const createImageElement = (src: string): HTMLImageElement => {
   const image = new Image();
-  image.src = src;
+  image.onload = () => {
+    image.src = src;
+  };
 
   return image;
 };
