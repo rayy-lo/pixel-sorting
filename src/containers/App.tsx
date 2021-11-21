3c58bf312fc3c08d9a6a0d131d44698718b59e09
Raymond <raymond.lo@yahoo.com>
2021-11-21 15:33:58 -0500

    import useImage hook

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,17 @@
 import Sidebar from "./Sidebar";
 import "../styles/App.css";
 import { Canvas } from "./Canvas/Canvas";
+import { useImage } from "../hooks/useImage";
+import kittenImg from "../assets/kitten.jpg";
 
 function App() {
+  const imageData = useImage(kittenImg);
+  console.log(imageData);
+
   return (
     <div className="App">
       <Sidebar />
-      <Canvas />
+      {imageData != null ? <Canvas imageData={imageData.data} /> : null}
     </div>
   );
 }
