d17d667b0f3c0085805d87e469598b83b0bc38fe
Raymond <raymond.lo@yahoo.com>
2021-11-21 20:03:10 -0500

    Get image data from an image src

---
+++ src/hooks/useMemoryCanvas.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useMemoryCanvas = (src: string) => {
+  const [imageData, setImageData] = useState<ImageData>();
+
+  useEffect(() => {
+    const handleOnload = () => {
+      const inMemoryCanvas = document.createElement("canvas");
+      const ctx = inMemoryCanvas.getContext("2d");
+
+      inMemoryCanvas.width = image.naturalWidth;
+      inMemoryCanvas.height = image.naturalHeight;
+
+      ctx?.drawImage(image, 0, 0);
+      const imageData = ctx?.getImageData(
+        0,
+        0,
+        image.naturalWidth,
+        image.naturalHeight
+      );
+      if (imageData) {
+        setImageData(imageData);
+      }
+    };
+
+    const image = new Image();
+    image.onload = handleOnload;
+    image.src = src;
+  }, []);
+
+  return imageData;
+};
