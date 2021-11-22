d547ae63714735e462ff7a80ebc9941213e40bcc
Raymond <raymond.lo@yahoo.com>
2021-11-21 20:04:18 -0500

    Pass imageData into canvas component

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,17 +1,22 @@
 import Sidebar from "./Sidebar";
 import "../styles/App.css";
 import { Canvas } from "./Canvas/Canvas";
-import { useImage } from "../hooks/useImage";
+import { useMemoryCanvas } from "../hooks/useMemoryCanvas";
 import kittenImg from "../assets/kitten.jpg";
 
+// 1. create image with src
+// 2. once created, draw image on canvas
+// 3. getImageData from canvas
+// 4. Shuffle pixelArray
+// 5. pass pixel array to render on canvas
+
 function App() {
-  const imageData = useImage(kittenImg);
-  console.log(imageData);
+  const imageData = useMemoryCanvas(kittenImg);
 
   return (
     <div className="App">
       <Sidebar />
-      {imageData != null ? <Canvas imageData={imageData.data} /> : null}
+      {imageData ? <Canvas imageData={imageData} /> : null}
     </div>
   );
 }
