117bc15ab100913c33fdfe62c0e1978e095908ea
Raymond <raymond.lo@yahoo.com>
2021-11-21 20:03:54 -0500

    Render imageData on canvas when component mounts

--- src/containers/Canvas/Canvas.tsx
+++ src/containers/Canvas/Canvas.tsx
@@ -1,36 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styles from "../../styles/modules/Canvas.module.css";
-export interface CanvasProp {
+
+interface CanvasProp {
   /**
    * Width of canvas
-   * @defaultValue 800
+   * @defaultValue 1080
    */
   width?: number;
   /**
    * Height of canvas
-   * @defaultValue 800
+   * @defaultValue 1080
    */
   height?: number;
   /**
-   * Pixel array to render on canvas
+   * Image data to render on canvas
    */
-  imageData: Uint8Array;
+  imageData: ImageData;
 }
 
 export const Canvas = ({
   imageData,
-  width = 800,
-  height = 800,
+  width = 1080,
+  height = 1080,
 }: CanvasProp) => {
   const { container, canvas } = styles;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    console.log("render pixel array onto canvas", imageData);
+    const ctx = canvasRef.current?.getContext("2d");
+    ctx?.putImageData(imageData, 0, 0);
   }, []);
 
   return (
     <div className={container}>
       <canvas
+        ref={canvasRef}
         className={canvas}
         width={width}
         height={height}
