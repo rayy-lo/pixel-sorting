67b7dd8fed93d5aa0946520f180f5f750265d7f7
Raymond <raymond.lo@yahoo.com>
2021-11-23 00:12:54 -0500

    Removed unused functions

--- src/containers/Canvas/index.ts
+++ src/containers/Canvas/index.ts
@@ -1,63 +1,22 @@
 /**
- * Get ImageData from image using an in memory canvas
+ * Shuffles image data - Uint8ClampedArray
  *
- * @param image
- *
- * @param tileWidth - Width of each tile
- *
- * @param tileHeight - Height of each tile
+ * @param data - Image data: Uint8ClampedArray
  *
  * @returns Uint8ClampedArray
  */
 
-export const getImageData = (
-  image: HTMLImageElement,
-  tileWidth: number = 32,
-  tileHeight: number = 32
-) => {
-  const inMemoryCanvas = document.createElement("canvas");
-  const ctx = inMemoryCanvas.getContext("2d");
+export const getShuffledImageData = (data: Uint8ClampedArray | undefined) => {
+  if (!data) return;
 
-  ctx?.drawImage(image, 0, 0);
-  const imageData = ctx?.getImageData(0, 0, tileWidth, tileHeight);
+  const uint8arr = new Uint8ClampedArray(data);
 
-  return imageData;
-};
-
-/**
- * Generate an image element for Canvas component to render
- * @async
- *
- * @param src - String source of image
- *
- * @returns HTMLImageElement
- */
-
-export const createImageElement = (
-  src: string,
-  callback: () => void
-): HTMLImageElement => {
-  const image = new Image();
-
-  image.onload = callback;
-  image.src = src;
-
-  return image;
-};
-
-/**
- * Shuffles array
- *
- * @param data - Canvas data: Uint8ClampedArray
- *
- * @returns Uint8ClampedArray
- */
-
-export const shuffleArray = (data: Uint8ClampedArray) => {
-  for (let i = data.length - 1; i > 0; i--) {
+  for (let i = uint8arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    const temp = data[i];
-    data[i] = data[j];
-    data[j] = temp;
+    const temp = uint8arr[i];
+    uint8arr[i] = uint8arr[j];
+    uint8arr[j] = temp;
   }
+
+  return new ImageData(uint8arr, 1080, 1080);
 };
