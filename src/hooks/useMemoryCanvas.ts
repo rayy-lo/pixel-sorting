84d5300d4e1f6ad748ad99d2da723ac2798f1cd5
Raymond <raymond.lo@yahoo.com>
2021-11-23 00:12:28 -0500

    Removed unused useref

--- src/hooks/useMemoryCanvas.ts
+++ src/hooks/useMemoryCanvas.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 export const useMemoryCanvas = (src: string) => {
   const [imageData, setImageData] = useState<ImageData>();
