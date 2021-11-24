b49dfa1d5d694c9a178993b58efbc633d9bd7f51
Raymond <raymond.lo@yahoo.com>
2021-11-24 00:14:22 -0500

    Render shuffled image data on load

--- src/containers/Canvas/Canvas.tsx
+++ src/containers/Canvas/Canvas.tsx
@@ -4,31 +4,38 @@ import styles from "../../styles/modules/Canvas.module.css";
 interface CanvasProp {
   /**
    * Width of canvas
-   * @defaultValue 1080
+   * @defaultValue 800
    */
   width?: number;
   /**
    * Height of canvas
-   * @defaultValue 1080
+   * @defaultValue 800
    */
   height?: number;
   /**
-   * Image data to render on canvas
+   * Shuffled image data to render on canvas
+   */
+  shuffledData: ImageData;
+  /**
+   * Sorted image data of image
    */
   imageData: ImageData;
 }
 
 export const Canvas = ({
   imageData,
-  width = 1080,
-  height = 1080,
+  shuffledData,
+  width = 800,
+  height = 800,
 }: CanvasProp) => {
   const { container, canvas } = styles;
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     const ctx = canvasRef.current?.getContext("2d");
-    ctx?.putImageData(imageData, 0, 0);
+    if (shuffledData) {
+      ctx?.putImageData(shuffledData, 0, 0);
+    }
   }, []);
 
   return (
