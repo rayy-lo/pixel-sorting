04be27132a9c285ce0537a72fea343900d7b8bd7
Raymond <raymond.lo@yahoo.com>
2021-11-24 00:13:53 -0500

    Got shuffled image data and passed to Canvas component

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,20 +3,22 @@ import "../styles/App.css";
 import { Canvas } from "./Canvas/Canvas";
 import { useMemoryCanvas } from "../hooks/useMemoryCanvas";
 import kittenImg from "../assets/kitten.jpg";
-
-// 1. create image with src
-// 2. once created, draw image on canvas
-// 3. getImageData from canvas
-// 4. Shuffle pixelArray
-// 5. pass pixel array to render on canvas
+import {
+  associateDataWithIndex,
+  getShuffledImageData,
+} from "../containers/Canvas";
+import { useEffect } from "react";
 
 function App() {
   const imageData = useMemoryCanvas(kittenImg);
+  const shuffledData = getShuffledImageData(imageData?.data!)!;
 
   return (
     <div className="App">
       <Sidebar />
-      {imageData ? <Canvas imageData={imageData} /> : null}
+      {imageData ? (
+        <Canvas imageData={imageData} shuffledData={shuffledData} />
+      ) : null}
     </div>
   );
 }
