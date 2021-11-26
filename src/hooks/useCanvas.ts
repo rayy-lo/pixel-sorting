50f6d86d309f9fa375425bad17b09e257af7b032
Raymond <raymond.lo@yahoo.com>
2021-11-26 16:14:20 -0500

    Call sorting algo in interval

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -2,27 +2,25 @@ import { useRef, useEffect } from "react";
 import {
   associateDataWithIndex,
   getShuffledImageData,
-  shuffleIndexedData,
 } from "../containers/Canvas";
-import { simpleSort } from "../containers/Canvas/sorting";
+import { quadraticSort } from "../containers/Canvas/sorting";
 
 export const useCanvas = (imageData: ImageData) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const intervalRef = useRef(0);
   const indexedData = associateDataWithIndex(imageData.data);
-  const shuffledData = shuffleIndexedData(indexedData);
-  // const shuffledData = getShuffledImageData(
-  //   new Uint8ClampedArray(Object.values(indexedData))
-  // );
+  const shuffledData = getShuffledImageData(
+    new Uint8ClampedArray(Object.values(indexedData))
+  );
 
   useEffect(() => {
     const ctx = canvasRef.current?.getContext("2d");
-    // if (shuffledData) {
-    //   ctx?.putImageData(shuffledData, 0, 0);
-    // }
+    if (shuffledData) {
+      ctx?.putImageData(shuffledData.imageData, 0, 0);
+    }
 
     intervalRef.current = window.setInterval(() => {
-      // simpleSort(shuffledData);
+      quadraticSort(shuffledData.randomizedOrder);
     }, 10000);
   }, []);
 
