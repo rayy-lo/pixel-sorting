91d0233ad5c716e6127361cc85311c6421ff9b48
Raymond <raymond.lo@yahoo.com>
2021-11-26 16:12:05 -0500

    quadratic sort time complexity

--- src/containers/Canvas/sorting.ts
+++ src/containers/Canvas/sorting.ts
@@ -5,6 +5,12 @@
  *
  * @returns {ImageData}
  */
-export const simpleSort = (data: ImageData) => {
-  console.log(data);
+export const quadraticSort = (arr: number[]) => {
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      // [3, 1, 2, 4, 5]
+    }
+  }
 };
