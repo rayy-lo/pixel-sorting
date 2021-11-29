e5824f44f9e90cf1d9404f22bec6d6264817adc8
Raymond <raymond.lo@yahoo.com>
2021-11-28 22:46:47 -0500

    Quadratic sorting algo

--- src/containers/Canvas/sorting.ts
+++ src/containers/Canvas/sorting.ts
@@ -6,11 +6,15 @@
  * @returns {ImageData}
  */
 export const quadraticSort = (arr: number[]) => {
-  console.log(arr);
-
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.length; j++) {
-      // [3, 1, 2, 4, 5]
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
     }
   }
+
+  console.log(arr);
 };
