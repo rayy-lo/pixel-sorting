df2273f7ab3cce1f23259cb4a275e09709a15e67
Raymond <raymond.lo@yahoo.com>
2021-11-30 02:59:57 -0500

    Add grid size state

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,12 +2,14 @@ import Sidebar from "./Sidebar";
 import "../styles/App.css";
 import { Canvas } from "./Canvas/Canvas";
 import kittenImg from "../assets/kitten.jpg";
+import { useState } from "react";
 
 function App() {
+  const [gridSize, setGridSize] = useState(5);
   return (
     <div className="App">
       <Sidebar />
-      <Canvas imageSrc={kittenImg} />
+      <Canvas gridSize={gridSize} imageSrc={kittenImg} />
     </div>
   );
 }
