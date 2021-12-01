5f36e377dd29145cc6e94657798edc90e9be1370
Raymond <raymond.lo@yahoo.com>
2021-12-01 00:37:03 -0500

    Shuffle pieces

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -1,25 +1,27 @@
 import { useRef, useEffect } from "react";
-import { generatePieces } from "../containers/Canvas";
+import { generatePieces, shuffle } from "../containers/Canvas";
 import { quadraticSort } from "../containers/Canvas/sorting";
 
 export const useCanvas = (imageSrc: string, gridSize: number) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const pieces = generatePieces(gridSize);
+  shuffle(pieces);
 
   useEffect(() => {
+    console.log("effect hook 1");
     const ctx = canvasRef.current?.getContext("2d");
     const handleImageLoad = () => {
-      var cw = canvasRef.current?.width;
-      var ch = canvasRef.current?.height;
-      var rows = gridSize;
-      var cols = gridSize;
-      var count = 0;
-      var pieceWidth = cw! / cols;
-      var pieceHeight = ch! / rows;
+      const cw = canvasRef.current?.width;
+      const ch = canvasRef.current?.height;
+      const rows = gridSize;
+      const cols = gridSize;
+      const pieceWidth = cw! / cols;
+      const pieceHeight = ch! / rows;
+      let count = 0;
 
       for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-          var piece = pieces[count++];
+          const piece = pieces[count++];
 
           ctx?.drawImage(
             img,
@@ -36,10 +38,14 @@ export const useCanvas = (imageSrc: string, gridSize: number) => {
       }
     };
 
-    var img = new Image();
+    const img = new Image();
     img.onload = handleImageLoad;
     img.src = imageSrc;
   }, []);
+
+  useEffect(() => {
+    console.log("effect hook 2");
+  });
 
   return canvasRef;
 };
