9b663c1b2480b33d6d12276e2d01a5e5950042d5
Raymond <raymond.lo@yahoo.com>
2021-12-02 18:22:33 -0500

    Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/modules/Button.module.css";
+
+interface ButtonProp {
+  children: React.ReactNode;
+}
+
+export const Button = () => {
+  return <button>Start</button>;
+};
