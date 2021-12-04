4f8d2ebbc2c316ba530d2d8cdf1bddfa703ac9ef
Raymond <raymond.lo@yahoo.com>
2021-12-04 04:45:01 -0500

    Add Button component

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,17 +1,23 @@
+import { Button } from "../components/Button";
 import { Select } from "../components/Select";
 import styles from "../styles/modules/Header.module.css";
-
 interface HeaderProp {
   handleChange: React.ChangeEventHandler;
+  isSorting: boolean;
 }
 
-const options: string[] = ["32", "64", "128"];
+const gridSizeOptions: string[] = ["32", "64", "128"];
 
-function Sidebar({ handleChange }: HeaderProp) {
+function Sidebar({ handleChange, isSorting }: HeaderProp) {
   return (
     <div className={styles.sidebar}>
-      <label htmlFor="">Size of Grid:</label>
-      <Select handleChange={handleChange} options={options} />
+      <Select
+        labelText="Select Grid Size: "
+        htmlFor="grid-size"
+        handleChange={handleChange}
+        options={gridSizeOptions}
+      />
+      <Button>{isSorting ? "Pause" : "Start"}</Button>
     </div>
   );
 }
