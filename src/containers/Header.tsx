ee8154f34abb961d9ffb77f4b7df31c16b24552b
Raymond <raymond.lo@yahoo.com>
2021-12-07 13:19:30 -0500

    Pass onclick handler to button

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -3,30 +3,33 @@ import { Select } from "../components/Select";
 import styles from "../styles/modules/Header.module.css";
 interface HeaderProp {
   handleChange: React.ChangeEventHandler;
+  sortImage: () => void;
   isSorting: boolean;
 }
+
+const { header } = styles;
 
 const gridSizeOptions = ["32", "64", "128"];
 const timeComplexityOptions = ["quadratic", "linear"];
 
-function Sidebar({ handleChange, isSorting }: HeaderProp) {
+function Header({ sortImage, handleChange, isSorting }: HeaderProp) {
   return (
-    <div className={styles.sidebar}>
+    <div className={header}>
       <Select
         labelText="Grid Size: "
-        htmlFor="grid-size"
+        htmlFor="numOfSquaresPerSide"
         handleChange={handleChange}
         options={gridSizeOptions}
       />
       <Select
         labelText="Time Complexity: "
-        htmlFor="time-complexity"
+        htmlFor="timeComplexity"
         handleChange={handleChange}
         options={timeComplexityOptions}
       />
-      <Button>{isSorting ? "Pause" : "Start"}</Button>
+      <Button handleClick={sortImage}>{isSorting ? "Pause" : "Start"}</Button>
     </div>
   );
 }
 
-export default Sidebar;
+export default Header;
