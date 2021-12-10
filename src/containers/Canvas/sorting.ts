85df5af49f3c61e943f77b48b434502bd8e99df9
Raymond <raymond.lo@yahoo.com>
2021-12-09 23:28:01 -0500

    Add ctx to sorting fn

--- src/containers/Canvas/sorting.ts
+++ src/containers/Canvas/sorting.ts
@@ -1,18 +1,24 @@
 import { Piece } from "../../types";
 
 /**
- * Sort pixel data - Time Complexity O(n^2)
+ * Sort Pieces Array In Place
  *
  * @param {Piece[]} piecesArray
  *
  */
-export const quadraticSort = (piecesArray: Piece[]) => {
+export const quadraticSort = (
+  piecesArray: Piece[],
+  ctx: CanvasRenderingContext2D
+) => {
+  console.log(piecesArray);
   for (let i = 0; i < piecesArray.length; i++) {
     for (let j = i + 1; j < piecesArray.length; j++) {
-      if (piecesArray[j] < piecesArray[i]) {
+      if (piecesArray[j].pieceNum < piecesArray[i].pieceNum) {
         const temp = piecesArray[i];
         piecesArray[i] = piecesArray[j];
         piecesArray[j] = temp;
+
+        ctx.putImageData(piecesArray[i].imageData!, 0, 0);
       }
     }
   }
