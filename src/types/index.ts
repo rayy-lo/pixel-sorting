622ed5811c3f0b93b41b96e72a19bab6eb56a7b1
Raymond <raymond.lo@yahoo.com>
2021-12-24 23:44:02 -0500

    add: canvas config type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,11 @@ export interface Piece {
   col: number
   row: number
   pieceNum: number
-  imageData?: ImageData
+}
+
+export interface CanvasConfig {
+  squares: 8 |16 | 32 | 64,
+  timeComplexity: 'quadratic' | 'linear'
+  height: 500 | 600 | 700 | 800,
+  width: 500 | 600 | 700 | 800
 }
