2b734b0888c3286e5e604f0c50f3c3403cacaff4
Raymond <raymond.lo@yahoo.com>
2021-12-25 02:08:14 -0500

    add: worker

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,19 +1,26 @@
 import { useState, ChangeEvent } from 'react'
+
 import Header from './Header'
-import '../styles/App.css'
-import { Canvas } from './Canvas/Canvas'
+
+import { Canvas } from './Canvas'
+import { CanvasConfig } from '../types'
+import { useCanvas } from '../utils/useCanvas'
+
 import kittenImg from '../assets/kitten.jpg'
-import { useCanvas } from '../hooks/useCanvas'
-// import { quadraticSort } from './Canvas/sorting'
+import '../styles/App.css'
 
 const App = () => {
-  const [canvasConfig, setCanvasConfig] = useState({
-    numOfSquaresPerSide: 16,
+  const [canvasConfig, setCanvasConfig] = useState<CanvasConfig>({
+    /**
+     * Number of rows and columns to divide canvas
+     */
+    squares: 16,
     timeComplexity: 'quadratic',
+    height: 600,
+    width: 600,
   })
-  const [isSorting, setIsSorting] = useState(false)
 
-  const [canvasRef, pieces] = useCanvas(kittenImg, canvasConfig.numOfSquaresPerSide)
+  const [canvasRef, pieces] = useCanvas(kittenImg, canvasConfig.squares)
 
   const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const { value } = event.target
@@ -23,16 +30,52 @@ const App = () => {
     })
   }
 
-  const sortImage = async () => {
-    // const ctx = canvasRef.current?.getContext("2d");
+  const sortImage = () => {
+    const rows = 16
+    const cols = 16
+    const pieceWidth = canvasConfig.width / cols
+    const pieceHeight = canvasConfig.height / rows
+    const ctx = canvasRef.current?.getContext('2d')
+
     const worker = new Worker(new URL('../workers/sorting.ts', import.meta.url))
-    worker.postMessage(pieces)
+
+    worker.postMessage([canvasConfig.timeComplexity, pieces])
+    worker.onmessage = (e) => {
+      // update canvas
+      const offscreen = new OffscreenCanvas(canvasConfig.width, canvasConfig.height)
+      const offCtx = offscreen.getContext('2d')
+      offCtx?.drawImage(canvasRef.current as HTMLCanvasElement, 0, 0)
+
+      ctx?.drawImage(
+        offscreen,
+        e.data[0].col * pieceWidth,
+        e.data[0].row * pieceHeight,
+        pieceWidth,
+        pieceHeight,
+        e.data[1].col * pieceWidth,
+        e.data[1].row * pieceHeight,
+        pieceWidth,
+        pieceHeight
+      )
+
+      ctx?.drawImage(
+        offscreen,
+        e.data[1].col * pieceWidth,
+        e.data[1].row * pieceHeight,
+        pieceWidth,
+        pieceHeight,
+        e.data[0].col * pieceWidth,
+        e.data[0].row * pieceHeight,
+        pieceWidth,
+        pieceHeight
+      )
+    }
   }
 
   return (
     <div className="App">
-      <Header handleChange={handleChange} sortImage={sortImage} isSorting={isSorting} />
-      <Canvas ref={canvasRef} />
+      <Header handleChange={handleChange} sortImage={sortImage} />
+      <Canvas height={canvasConfig.height} width={canvasConfig.width} ref={canvasRef} />
     </div>
   )
 }
