a6279f37734891215bdd9e20cc13111be2e309fd
Raymond <raymond.lo@yahoo.com>
2021-12-24 23:45:21 -0500

    fix: quad sort algo

--- src/workers/sorting.ts
+++ src/workers/sorting.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-restricted-globals */
 import { Piece } from '../types/index'
 
@@ -5,19 +6,17 @@ const quadraticSort = (pieces: Piece[]) => {
     for (let i = 0; i < pieces.length; i++) {
       for (let j = i + 1; j < pieces.length; j++) {
         if (pieces[j].pieceNum < pieces[i].pieceNum) {
-        
-          
-            const temp = pieces[i];
-            
-        //   pieces[i] = pieces[j]
-        //   pieces[j] = temp
+          postMessage([pieces[i], pieces[j]])
+
+          const temp = pieces[i];
+          pieces[i] = pieces[j]
+          pieces[j] = temp
         }
       }
   }
 }
 
 self.onmessage = (e) => {
-    console.log(e);
     quadraticSort(e.data);
 }
 
