990b86bb6933681287e1154412b62f2e617ce758
Raymond <raymond.lo@yahoo.com>
2022-01-02 10:55:47 -0500

    Add button type prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import '../styles/modules/Button.module.css'
+import styles from '../styles/modules/Button.module.css'
 
 interface ButtonProp {
   children: React.ReactNode
   handleClick: React.MouseEventHandler
+  type: 'button' | 'submit'
 }
 
-export const Button = ({ children, handleClick }: ButtonProp) => {
+const { button } = styles
+
+export const Button = ({ type, children, handleClick }: ButtonProp) => {
   return (
-    <button type="button" onClick={handleClick}>
+    <button className={button} type={type === 'button' ? 'button' : 'submit'} onClick={handleClick}>
       {children}
     </button>
   )
