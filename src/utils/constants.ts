619b67a53ab5aa3012fbb3285cebb5f2626c7898
Raymond <raymond.lo@yahoo.com>
2022-01-02 11:06:01 -0500

    add image src array

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,11 @@
-export const squareOptions = [10, 15, 20]
+import kitten from '../assets/images/kitten.jpeg'
+import golden from '../assets/images/golden.jpeg'
+import bird from '../assets/images/bird.jpeg'
+import eiffel from '../assets/images/eiffel.jpeg'
+import rose from '../assets/images/rose.jpeg'
+
+export const images = [kitten, golden, bird, eiffel, rose];
+export const squareOptions = [5, 10, 15]
 export const timeOptions = ['quadratic', 'linear']
+export const canvasWidth = 600;
+export const canvasHeight = 600;
