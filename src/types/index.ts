df74fa67b660c82dc22d479c00b0f91405e5b522
Raymond <raymond.lo@yahoo.com>
2022-01-05 21:22:24 -0500

    edit canvas config inter

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,7 +6,7 @@ export interface Piece {
 
 export interface CanvasConfig {
   squares: 5 | 10 | 15
-  timeComplexity: 'quadratic' | 'linear'
+  sortingMethod: 'naive' | 'selection sort'
   height: 600
   width: 600
 }
