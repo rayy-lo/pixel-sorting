851939d1441f3d0ae165890321e073949d98c99c
Raymond <raymond.lo@yahoo.com>
2022-02-07 22:59:37 -0500

    Change path of App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './containers/App'
+import App from './pages/App'
 import './styles/index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 )
