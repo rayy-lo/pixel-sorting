fd040673bf6b525d96af423f667200a0d4f7fcff
Raymond <raymond.lo@yahoo.com>
2022-02-09 11:36:37 -0500

    renamed change handler

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,37 +2,37 @@ import { ChangeEventHandler } from 'react'
 import styles from '../styles/modules/Select.module.css'
 
 interface SelectProp {
-  /**
-   * options to render for select
-   */
-  options: string[] | number[]
-  handleChange: ChangeEventHandler
-  htmlFor: string
-  labelText: string
-  disabled: boolean
+    /**
+     * options to render for select
+     */
+    options: string[] | number[]
+    onConfigChange: ChangeEventHandler
+    htmlFor: string
+    labelText: string
+    disabled: boolean
 }
 
 const { container, label } = styles
 
-export const Select = ({ disabled, labelText, options, handleChange, htmlFor }: SelectProp) => {
-  return (
-    <div className={container}>
-      <label className={label} htmlFor={htmlFor}>
-        {labelText}
-      </label>
-      <select
-        disabled={disabled}
-        aria-disabled={disabled}
-        name={htmlFor}
-        id={htmlFor}
-        onChange={handleChange}
-      >
-        {options.map((option) => (
-          <option value={option} key={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  )
+export const Select = ({ disabled, labelText, options, onConfigChange, htmlFor }: SelectProp) => {
+    return (
+        <div className={container}>
+            <label className={label} htmlFor={htmlFor}>
+                {labelText}
+            </label>
+            <select
+                disabled={disabled}
+                aria-disabled={disabled}
+                name={htmlFor}
+                id={htmlFor}
+                onChange={onConfigChange}
+            >
+                {options.map((option) => (
+                    <option value={option} key={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
 }
