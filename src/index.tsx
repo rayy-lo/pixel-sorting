6e776fc33b1e87303e862a33b9955bb1425dde05
Raymond <raymond.lo@yahoo.com>
2022-02-10 17:58:16 -0500

    Add mobile and browser views

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserView, MobileView } from 'react-device-detect'
 import App from './pages/App'
 import './styles/index.css'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserView>
+            <App />
+        </BrowserView>
+        <MobileView>
+            <h1>Mobile is not supported for this app</h1>
+        </MobileView>
     </React.StrictMode>,
     document.getElementById('root')
 )
