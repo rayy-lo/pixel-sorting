cb2aa57283851d575a6e57279f93f378cb6b3750
Raymond <raymond.lo@yahoo.com>
2022-02-28 03:24:52 -0500

    add timer hook

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,8 +1,23 @@
 import { useState, useEffect } from "react"
 
+export const useTimer = (startTime: Date | null) => {
+    const [endTime, setEndTime] = useState<Date | null>(null);
+    let minutes = '00'
+    let seconds = '00'
 
-export const useTimer = (gameStarted) => {
-    const [now, setNow] = useState<Date | null>(null)
+    if(endTime && startTime) {
+        seconds = `${(Math.floor(Math.abs(endTime.getTime() - startTime.getTime()) / (1000) % 60))}`.padStart(2, '0')
+        minutes = `${(Math.floor(Math.abs(endTime.getTime() - startTime.getTime()) / (1000 * 60) % 60))}`.padStart(2, '0')
+    }
+
+
+    useEffect(() => {
+        if(startTime){
+            const id = setInterval(() => {
+                setEndTime(new Date());
+            }, 1000)
+        }    
+    }, [startTime])
 
     return {
         minutes,
