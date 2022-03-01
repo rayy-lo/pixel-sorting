29b48f22e557a36abaeb5474b31bdf13c09e24c9
Raymond <raymond.lo@yahoo.com>
2022-03-01 18:39:08 -0500

    call useworker hook

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,11 @@
 import { useState, ChangeEvent } from 'react'
-
-import Header from '../components/Header'
-import { MemoCanvas } from '../components/Canvas/Canvas'
 import { CanvasConfig } from '../types'
 import { useCanvas } from '../hooks/useCanvas'
+import { useWorker } from '../hooks/useWorker'
+// import { canvasHeight, canvasWidth } from '../utils/constants'
+import { MemoCanvas } from '../components/Canvas/Canvas'
 import AnswerPanel from '../components/AnswerPanel/AnswerPanel'
-import { canvasHeight, canvasWidth } from '../utils/constants'
+import GameSettings from '../components/GameSettings/GameSettings'
 
 const App = () => {
     const [canvasConfig, setCanvasConfig] = useState<CanvasConfig>({
@@ -14,8 +14,9 @@ const App = () => {
     })
 
     const [canvasRef, pieces] = useCanvas(canvasConfig.squares)
+    const { stopSort, startSort } = useWorker(canvasConfig, canvasRef, pieces)
 
-    const worker = new Worker(new URL('../workers/sorting.ts', import.meta.url))
+    // const worker = new Worker(new URL('../workers/sorting.ts', import.meta.url))
 
     const onConfigChange = (event: ChangeEvent<HTMLSelectElement>) => {
         const { value } = event.target
@@ -25,53 +26,53 @@ const App = () => {
         })
     }
 
-    const stopSort = () => worker.terminate()
+    // const stopSort = () => worker.terminate()
 
-    const startSort = () => {
-        const pieceWidth = canvasWidth / canvasConfig.squares
-        const pieceHeight = canvasHeight / canvasConfig.squares
-        const ctx = canvasRef.current?.getContext('2d')
+    // const startSort = () => {
+    //     const pieceWidth = canvasWidth / canvasConfig.squares
+    //     const pieceHeight = canvasHeight / canvasConfig.squares
+    //     const ctx = canvasRef.current?.getContext('2d')
 
-        worker.postMessage([canvasConfig.sortingMethod, pieces])
-        worker.onmessage = (e) => {
-            const firstPiece = e.data[0]
-            const secondPiece = e.data[1]
+    //     worker.postMessage([canvasConfig.sortingMethod, pieces])
+    //     worker.onmessage = (e) => {
+    //         const firstPiece = e.data[0]
+    //         const secondPiece = e.data[1]
 
-            const offscreen = new OffscreenCanvas(canvasWidth, canvasHeight)
-            const offCtx = offscreen.getContext('2d')
-            offCtx?.drawImage(canvasRef.current as HTMLCanvasElement, 0, 0)
+    //         const offscreen = new OffscreenCanvas(canvasWidth, canvasHeight)
+    //         const offCtx = offscreen.getContext('2d')
+    //         offCtx?.drawImage(canvasRef.current as HTMLCanvasElement, 0, 0)
 
-            ctx?.drawImage(
-                offscreen,
-                firstPiece.col * pieceWidth,
-                firstPiece.row * pieceHeight,
-                pieceWidth,
-                pieceHeight,
-                secondPiece.col * pieceWidth,
-                secondPiece.row * pieceHeight,
-                pieceWidth,
-                pieceHeight
-            )
+    //         ctx?.drawImage(
+    //             offscreen,
+    //             firstPiece.col * pieceWidth,
+    //             firstPiece.row * pieceHeight,
+    //             pieceWidth,
+    //             pieceHeight,
+    //             secondPiece.col * pieceWidth,
+    //             secondPiece.row * pieceHeight,
+    //             pieceWidth,
+    //             pieceHeight
+    //         )
 
-            ctx?.drawImage(
-                offscreen,
-                secondPiece.col * pieceWidth,
-                secondPiece.row * pieceHeight,
-                pieceWidth,
-                pieceHeight,
-                firstPiece.col * pieceWidth,
-                firstPiece.row * pieceHeight,
-                pieceWidth,
-                pieceHeight
-            )
-        }
-    }
+    //         ctx?.drawImage(
+    //             offscreen,
+    //             secondPiece.col * pieceWidth,
+    //             secondPiece.row * pieceHeight,
+    //             pieceWidth,
+    //             pieceHeight,
+    //             firstPiece.col * pieceWidth,
+    //             firstPiece.row * pieceHeight,
+    //             pieceWidth,
+    //             pieceHeight
+    //         )
+    //     }
+    // }
 
     return (
         <div className="App">
-            <Header onConfigChange={onConfigChange} />
             <MemoCanvas ref={canvasRef} />
             <AnswerPanel />
+            <GameSettings />
         </div>
     )
 }
